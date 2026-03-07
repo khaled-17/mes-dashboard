e729679d82ab56f249cbe47348424d47e545ff40
kerllouskhairy987 <kyrlskhyry80@gmail.com && git config --global user.name kerllouskhairy987git config --global user.email kyrlskhyry80@gmail.com>
2026-03-07 16:28:21 +0200

    create structure for login page and create khairy-login branch

---
+++ view/login/components/LoginForm.tsx
@@ -0,0 +1,11 @@
+
+const LoginForm = () => {
+    return (
+        <div className="max-w-480 w-full">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, repellat provident, totam exercitationem deserunt possimus facilis nisi in placeat modi ipsam. Quod 
+            </p>
+        </div>
+    )
+}
+
+export default LoginForm
