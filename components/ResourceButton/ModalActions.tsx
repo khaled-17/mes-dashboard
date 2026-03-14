0cb3babd423e46915c78dde8e105a7e364c61473
Abdullah <abdullahxyzabc67@gmail.com>
2026-03-14 14:08:50 +0300

    feat : create Resource Button with shadcn

---
+++ components/ResourceButton/ModalActions.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "../ui/button";
+import { DialogClose } from "../ui/dialog";
+
+function ModalActions() {
+  return (
+    <div className=" ml-195 mr-20 mb-10 mt-25 flex gap-3  ">
+      <DialogClose asChild>
+        <Button className="hover:bg-primary hover:text-white bg-white text-primary border-[2px]   border-[#2d62ed] text-[13px] font-roboto font-medium   text-left py-3 px-8 cursor-pointer uppercase  rounded-sm">
+          Cancle
+        </Button>
+      </DialogClose>
+      <Button className="bg-primary  text-white p-2 text-left py-3 px-8 cursor-pointer rounded-sm uppercase ">
+        Add New
+      </Button>
+    </div>
+  );
+}
+
+export default ModalActions;
