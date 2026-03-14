eb8622df8ac6519b81e06a6ac5f8e422cf7515f9
Karim Sayed <karimsayed218m@gmail.com>
2026-03-14 17:13:56 +0200

    final edit dashboard

--- view/Home/components/side Card/shelf.tsx
+++ view/Home/components/side Card/shelf.tsx
@@ -10,18 +10,20 @@ interface shelfProps {
 function Shelf({icon, iconName, description, id}: shelfProps) {
   return (
     <div>
-        <div className="flex gap-3 items-center border-b border-border py-3">
-              <div className="flex flex-col items-center gap-0 flex-1">
-                {icon}
-                <p className='text-[#8D9192] text-[11px] font-bold'>{iconName}</p>
-              </div>
-              <div className='text-[#464646] text-[16px] font-bold flex-3'>
-                <p>{description}</p>
-                <div className='flex justify-between items-center'>
-                  <p className=''>ID</p>
-                  <p className='font-extrabold'>{id}</p>
+        <div>
+          <div className="flex gap-3 items-center border-b border-border py-3">
+                <div className="flex flex-col items-center gap-0 flex-1">
+                  {icon}
+                  <p className='text-[#8D9192] text-[11px] font-bold'>{iconName}</p>
                 </div>
-              </div>
+                <div className='text-[#464646] text-[16px] font-bold flex-3'>
+                  <p>{description}</p>
+                  <div className='flex justify-between items-center'>
+                    <p className=''>ID</p>
+                    <p className='font-extrabold'>{id}</p>
+                  </div>
+                </div>
+          </div>
         </div>
     </div>
   )
