9df47be80e7a80d22a5b4b3932a97e4a27a2c4b2
Karim Sayed <karimsayed218m@gmail.com>
2026-03-14 16:18:06 +0200

    tempo dashboard

---
+++ view/Home/components/TableCard/TableCard.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Activity, SlidersVertical, Route, Tag } from "lucide-react";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FunctionTab from "./Function Tab/F-Tab";
+import RouteTab from "./Route Tab/R-Tab";;
+import JobOrderTab from "./Job Orders Tab/JO-Tab";
+import { useState } from "react";
+
+function TableCard() {
+
+  const [activeTab, setActiveTab] = useState<string>("Function");
+
+  const handleTabChange = () => {
+    if (activeTab === "Function") {
+      return <FunctionTab />
+    }
+    else if (activeTab === "Route") {
+      return <RouteTab />
+    }
+    else if (activeTab === "Job Order") {
+      return <JobOrderTab />
+    }
+  }
+
+  return (
+    <div>
+      <Card className="w-310 h-165">   
+        <CardHeader className="px-7 flex items-center justify-between">
+          <CardTitle className="flex items-center gap-4 ">
+            <Activity className="w-5 h-5" />
+            <span className="text-[16px]">Current Activity</span>
+          </CardTitle>
+
+          <Tabs defaultValue="Function">
+            <TabsList variant="underline" className="border-none gap-5 mr-15">
+              <TabsTrigger value="Function" onClick={() => {setActiveTab("Function")}} className="pb-3 text-[16px]">
+                <SlidersVertical className="w-5! h-5! mr-1" />Function
+              </TabsTrigger>
+              <TabsTrigger value="Route" onClick={() => {setActiveTab("Route")}} className="pb-3 text-[16px]">
+                <Route className="w-5! h-5! mr-1" />Route
+              </TabsTrigger>
+              <TabsTrigger value="Job Order" onClick={() => {setActiveTab("Job Order")}} className="pb-3 text-[16px]">
+                <Tag className="w-5! h-5! mr-1" />Job Order
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
+        </CardHeader>
+
+        <CardContent className="pt-10 pb-[-30px] px-20">
+          {handleTabChange()}
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default TableCard
---
+++ view/Home/components/TopCards/charts.tsx
@@ -0,0 +1,174 @@
+"use client";
+import { ChartContainer } from "@/components/ui/chart";
+import {
+  chartConfig,
+  chartData1,
+  chartData2,
+  chartData3,
+} from "@/data/chartData";
+import {
+  RadialBarChart,
+  PolarGrid,
+  RadialBar,
+  PolarRadiusAxis,
+  Label,
+} from "recharts";
+
+function Charts() {
+  return (
+    <>
+        <div className="flex-1 flex flex-col items-center">
+            <ChartContainer config={chartConfig} className="aspect-square h-25">
+            <RadialBarChart
+                data={chartData1}
+                startAngle={90}
+                endAngle={-180}
+                innerRadius={28}
+                outerRadius={38}
+            >
+                <PolarGrid
+                gridType="circle"
+                radialLines={false}
+                stroke="none"
+                className="first:fill-muted last:fill-background"
+                polarRadius={[39, 30]}
+                />
+                <RadialBar dataKey="visitors" background cornerRadius={10} />
+                <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                <Label
+                    content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                        return (
+                        <text
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            textAnchor="middle"
+                            dominantBaseline="middle"
+                        >
+                            <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-[21px] font-bold"
+                            >
+                            {chartData1[0].visitors.toLocaleString()}%
+                            </tspan>
+                            <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                            ></tspan>
+                        </text>
+                        );
+                    }
+                    }}
+                />
+                </PolarRadiusAxis>
+            </RadialBarChart>
+            </ChartContainer>
+            <p className="font-bold text-[16px] -mt-2">Availability</p>
+        </div>
+        <div className="flex-1 flex flex-col items-center">
+            <ChartContainer config={chartConfig} className="aspect-square h-25">
+            <RadialBarChart
+                data={chartData2}
+                startAngle={90}
+                endAngle={-140}
+                innerRadius={28}
+                outerRadius={38}
+            >
+                <PolarGrid
+                gridType="circle"
+                radialLines={false}
+                stroke="none"
+                className="first:fill-muted last:fill-background"
+                polarRadius={[39, 30]}
+                />
+                <RadialBar dataKey="visitors" background cornerRadius={10} />
+                <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                <Label
+                    content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                        return (
+                        <text
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            textAnchor="middle"
+                            dominantBaseline="middle"
+                        >
+                            <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-[21px] font-bold"
+                            >
+                            {chartData2[0].visitors.toLocaleString()}%
+                            </tspan>
+                            <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                            ></tspan>
+                        </text>
+                        );
+                    }
+                    }}
+                />
+                </PolarRadiusAxis>
+            </RadialBarChart>
+            </ChartContainer>
+            <p className="font-bold text-[16px] -mt-2">Preformance</p>
+        </div>
+        <div className="flex-1 flex flex-col items-center">
+            <ChartContainer config={chartConfig} className="aspect-square h-25">
+            <RadialBarChart
+                data={chartData3}
+                startAngle={90}
+                endAngle={0}
+                innerRadius={28}
+                outerRadius={38}
+            >
+                <PolarGrid
+                gridType="circle"
+                radialLines={false}
+                stroke="none"
+                className="first:fill-muted last:fill-background"
+                polarRadius={[39, 30]}
+                />
+                <RadialBar dataKey="visitors" background cornerRadius={10} />
+                <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                <Label
+                    content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                        return (
+                        <text
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            textAnchor="middle"
+                            dominantBaseline="middle"
+                        >
+                            <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-[21px] font-bold"
+                            >
+                            {chartData3[0].visitors.toLocaleString()}%
+                            </tspan>
+                            <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                            ></tspan>
+                        </text>
+                        );
+                    }
+                    }}
+                />
+                </PolarRadiusAxis>
+            </RadialBarChart>
+            </ChartContainer>
+            <p className="font-bold text-[16px] -mt-2">Quality</p>
+        </div>
+    </>
+  );
+}
+
+export default Charts;
