9956b9b17593d139b67edaa9253584b525a09292
Mohamed Hamdy <mohammed.hamdi.mohamed@gmail.com>
2026-04-03 16:38:29 +0200

    Manufacturing-page-process-match-design

--- app/manufacturing/[tab]/page.tsx
+++ app/manufacturing/[tab]/page.tsx
@@ -1,6 +1,19 @@
 import ManufacturingPage from "@/view/Manufacturing/ManufacturingPage";
+import ProcessPage from "@/view/Manufacturing/ProcessPage/ProcessPage";
 
-const ManufacturingTabPage = () => {
+
+interface Props {
+  params: Promise<{ tab: string }>;
+}
+
+const ManufacturingTabPage = async ({ params }: Props) => {
+  const { tab } = await params;
+
+  if (tab === "process") {
+    return <ProcessPage />;
+  }
+
+
   return <ManufacturingPage />;
 };
 
